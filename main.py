a8d027e8fd1c5230c1869f4077b5f319a1042735
lijinfeng <728729140@qq.com>
2023-08-06 15:33:14 +0800

    init

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from wechatpy import WeChatClient
-from wechatpy.client.api import WeChatMessage, WeChatTemplate
+from wechatpy.client.api import WeChatMessage
 import os
 
-app_id = os.environ["APP_ID"]
-app_secret = os.environ["APP_SECRET"]
 
-user_id = os.environ["USER_ID"]
-template_id = os.environ["TEMPLATE_ID"]
+if __name__ == '__main__':
+    app_id = os.environ["APP_ID"]
+    app_secret = os.environ["APP_SECRET"]
 
-client = WeChatClient(app_id, app_secret)
+    user_id = os.environ["USER_ID"]
+    template_id = os.environ["TEMPLATE_ID"]
 
-wm = WeChatMessage(client)
-data = {}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+    client = WeChatClient(app_id, app_secret)
+
+    wm = WeChatMessage(client)
+    data = {}
+    res = wm.send_template(user_id, template_id, data)
+    print(res)
